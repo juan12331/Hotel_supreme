17d4bae4c7aab1dd4099b4cba2b3494e30070148
juan12331 <juanxadez2@gmail.com>
2024-10-03 14:51:11 -0300

    adicionando rotas dos usuarios

---
+++ backend/routes/routes.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const routes = express.Router();
+const UsuariosControllers = require('../controllers/users')
+const QuartosControllers = require('../controllers/quartos')
+const ImagensControllers = require('../controllers/Imagens')
+
+//função dos usuarios
+routes.get('/usuarios', UsuariosControllers.getUsers)
+routes.get('/usuarios/:cpf', UsuariosControllers.getUsersByCpf)
+
+routes.post('/login', UsuariosControllers.login)
+
+
+//função dos quartos
+
+
+//função das imagens
