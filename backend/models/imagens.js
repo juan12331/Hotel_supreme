fd600afd496d2ed5002cc6ca288201ef01ae4411
juan12331 <juanxadez2@gmail.com>
2024-10-03 14:35:51 -0300

    criando tabela imagens

---
+++ backend/models/imagens.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize')
+const database = require('../config/sequelize')
+const { default_type } = require('mime');
+const quartos = require('./quartos')
+
+const imagens = database.define('imagens', {
+
+    id_imagem: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    
+    imagem: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    id_quarto: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+});
+
+quartos.hasMany(imagens, {
+    foreignKey: 'id_autor'
+})
+
+imagens.belongsTo(quartos, {
+    foreignKey: 'id_autor'
+});
+
+module.exports = livros
